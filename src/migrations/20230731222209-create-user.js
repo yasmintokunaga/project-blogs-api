b164e19cf214103c353157ed171acf1bbfec4709
yasmintokunaga <yasmintkng@gmail.com>
2023-07-31 20:13:20 -0300

    Create the migrations for generate tables: users, categories, blog_posts, posts_categories

---
+++ src/migrations/20230731222209-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        field: 'display_name',
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  },
+};
---
+++ src/migrations/20230731224125-create-blog-post.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('blog_posts');
+  }
+};
