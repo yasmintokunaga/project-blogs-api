b198469727f4945fb50ca2d2d176290d315481ed
yasmintokunaga <yasmintkng@gmail.com>
2023-08-02 17:39:52 -0300

    feat(REQ08): create the enpoint POST /categories

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const login = require('./controllers/login.controller');
 const user = require('./controllers/user.controller');
+const category = require('./controllers/category.controller');
 const validateJWT = require('./auth/validateJWT');
 // ...
 
@@ -21,6 +22,8 @@ app.post('/user', user.validateUser, user.createUser);
 
 app.get('/user', validateJWT, user.getAll);
 app.get('/user/:id', validateJWT, user.getById);
+
+app.post('/categories', validateJWT, category.createCategory);
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
